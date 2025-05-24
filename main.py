9ceff2c79f558c9dc52dacf5cdfb37dae983ff42
Chara Dust <163882750+KARkitkat@users.noreply.github.com>
2025-05-24 20:34:28 +0800

    配置与调试主要代码

--- main.py
+++ main.py
@@ -5,48 +5,31 @@ import machine
 from machine import Pin
 from machine import I2C
 from machine import PWM
-from ssd1306 import SSD1306_I2C
 
 import network
 
 ssid = "ssid"
-passwd = "passwd"
+passwd = "password"
 
 p2 = Pin(2, Pin.OUT)  # create output pin on GPIO0
 
 wf = network.WLAN(network.STA_IF)
 wf.active(True)
 i2c = I2C(scl=Pin(5), sda=Pin(4))
-oled = SSD1306_I2C(128, 64, i2c)
 servo = PWM(machine.Pin(14), freq=50)
 servo_status=2
-
-
-def show_status():
-    oled.fill(0)
-    oled.text("SSID:" + wf.config("essid"), 0, 0)
-    oled.text("Status:" + str(wf.isconnected()), 0, 10)
-    if servo_status ==1:
-        oled.text("Servo On", 0, 20)
-    elif servo_status==0:
-        oled.text("Servo Off", 0, 20)
-    oled.show()
-
 
 def wifi_connect():
     p2.off()
     time.sleep_ms(50)
     p2.on()
     wf.connect(ssid, passwd)
-    oled.fill(0)
-    oled.text("wifi connecting", 0, 0)
-    oled.show()
     time.sleep(3)
 
     if not wf.isconnected():
         wifi_connect()
     else:
-        show_status()
+        print()
 
 
 wifi_connect()
@@ -70,36 +53,29 @@ while True:
     time.sleep_ms(50)
     p2.on()
     conn, addr = s.accept()
-    print("GOT a connection from %s" % str(addr))
     time.sleep_ms(200)
     request = conn.recv(2024)
-    print("Content %s" % str(request))
     request = str(request)
     servo_on = request.find('/?servo=ON')
     servo_off = request.find('/?servo=OFF')
-    print(servo_on,servo_off)
+    servo_keepon = request.find('/?servo=keepon')
 
     if (servo_on == 6):
         servo.duty(125)
         servo_status=1
-        show_status()
 
-        time.sleep(2)
+        time.sleep(3)
 
         servo.duty(25)
         servo_status = 0
-        show_status()
     if (servo_off == 6):
         servo.duty(25)
         servo_status=0
-        show_status()
+    if (servo_keepon == 6):
+        servo.duty(125)
+        servo_status=0
     servo_status = 2
-    show_status()
     # conn.send(b"HTTP/1.1 200 OK\r\n")
     conn.send(html)
     conn.close()
     machine.reset()
-
-
-
-
